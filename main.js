238cf38e868865726c6583202c16e215789301e4
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-29 21:50:09 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ inputEll.addEventListener("input", (e) => {
 // 2. При клике на кнопку с классом messageBtn необходимо элементу с классом message:
 // - добавить два класса: animate_animated и animate_fadeInLeftBig
 // - поставить данному элементу стиль visibility в значение 'visible'.
+const btnEll = document.querySelector(".messageBtn");
+
+btnEll.addEventListener("click", () => {
+  btnEll.classList.add("animate_animated", "animate_fadeInLeftBig");
+  btnEll.style.visibility = "visible";
+});
 
 // 3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме. Если какое-либо поле не заполнено, форма не должна отправляться, также должны быть подсвечены незаполненные поля (необходимо поставить класс error незаполненным полям). Как только пользователь начинает заполнять какое-либо поле, необходимо, при вводе в данное поле, произвести проверку:
 // - Если поле пустое, необходимо данное поле подсветить (поставить класс error данному полю).
